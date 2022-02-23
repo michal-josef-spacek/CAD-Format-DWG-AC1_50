a5801cf4f19468bc6f74fd1e580d3285a38b91d6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-02-24 00:26:55 +0100

    Rename version_major to unknown_3
    Variables is unknown and it values is mostly 3.

--- AC1_50.pm
+++ AC1_50.pm
@@ -3386,7 +3386,7 @@ sub _read {
     $self->{magic} = $self->{_io}->read_bytes(6);
     $self->{zeros} = $self->{_io}->read_bytes(6);
     $self->{zero_one_or_three} = $self->{_io}->read_s1();
-    $self->{version_major} = $self->{_io}->read_s2le();
+    $self->{unknown_3} = $self->{_io}->read_s2le();
     $self->{num_sections} = $self->{_io}->read_s2le();
     $self->{num_header_vars} = $self->{_io}->read_s2le();
     $self->{dwg_version} = $self->{_io}->read_s1();
@@ -3434,9 +3434,9 @@ sub zero_one_or_three {
     return $self->{zero_one_or_three};
 }
 
-sub version_major {
+sub unknown_3 {
     my ($self) = @_;
-    return $self->{version_major};
+    return $self->{unknown_3};
 }
 
 sub num_sections {
