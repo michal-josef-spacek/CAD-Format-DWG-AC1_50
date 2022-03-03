1d288d3337b533e3160bd2206bf71c899527a0d9
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-03-03 01:00:52 +0100

    Fix block_name

--- AC1_50.pm
+++ AC1_50.pm
@@ -2123,8 +2123,7 @@ sub _read {
     my ($self) = @_;
 
     $self->{flag} = CAD::Format::DWG::AC1_50::BlockFlag->new($self->{_io}, $self, $self->{_root});
-    $self->{block_name} = Encode::decode("ASCII", IO::KaitaiStruct::Stream::bytes_terminate($self->{_io}->read_bytes(31), 0, 0));
-    $self->{u1} = $self->{_io}->read_s1();
+    $self->{block_name} = Encode::decode("ASCII", IO::KaitaiStruct::Stream::bytes_terminate($self->{_io}->read_bytes(32), 0, 0));
     $self->{u2} = $self->{_io}->read_s1();
     $self->{u3} = $self->{_io}->read_s1();
     $self->{u4} = $self->{_io}->read_s1();
@@ -2139,11 +2138,6 @@ sub flag {
 sub block_name {
     my ($self) = @_;
     return $self->{block_name};
-}
-
-sub u1 {
-    my ($self) = @_;
-    return $self->{u1};
 }
 
 sub u2 {
