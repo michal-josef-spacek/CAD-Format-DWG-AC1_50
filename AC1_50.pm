2e77915847185fb1ac0bc45c757de1e573cb308c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-03-21 15:02:41 +0100

    Rewrite block size variables

--- AC1_50.pm
+++ AC1_50.pm
@@ -143,7 +143,7 @@ sub _read {
     for (my $i = 0; $i < $n_views; $i++) {
         $self->{views}[$i] = CAD::Format::DWG::AC1_50::View->new($self->{_io}, $self, $self->{_root});
     }
-    $self->{_raw_block_entities} = $self->{_io}->read_bytes($self->header()->blocks_size_b());
+    $self->{_raw_block_entities} = $self->{_io}->read_bytes($self->header()->blocks_size());
     my $io__raw_block_entities = IO::KaitaiStruct::Stream->new($self->{_raw_block_entities});
     $self->{block_entities} = CAD::Format::DWG::AC1_50::RealEntities->new($io__raw_block_entities, $self, $self->{_root});
     if (!($self->_io()->is_eof())) {
@@ -3387,7 +3387,7 @@ sub _read {
     $self->{entities_start} = $self->{_io}->read_u4le();
     $self->{entities_end} = $self->{_io}->read_u4le();
     $self->{blocks_start} = $self->{_io}->read_u4le();
-    $self->{blocks_size} = $self->{_io}->read_u4le();
+    $self->{blocks_size_raw} = $self->{_io}->read_u4le();
     $self->{blocks_end} = $self->{_io}->read_u4le();
     $self->{unknown4b} = $self->{_io}->read_bytes(2);
     $self->{unknown4c} = $self->{_io}->read_bytes(2);
@@ -3399,18 +3399,18 @@ sub _read {
     $self->{variables} = CAD::Format::DWG::AC1_50::HeaderVariables->new($self->{_io}, $self, $self->{_root});
 }
 
-sub blocks_size_a {
+sub blocks_size_unknown {
     my ($self) = @_;
-    return $self->{blocks_size_a} if ($self->{blocks_size_a});
-    $self->{blocks_size_a} = (($self->blocks_size() & 4278190080) >> 24);
-    return $self->{blocks_size_a};
+    return $self->{blocks_size_unknown} if ($self->{blocks_size_unknown});
+    $self->{blocks_size_unknown} = (($self->blocks_size_raw() & 4278190080) >> 24);
+    return $self->{blocks_size_unknown};
 }
 
-sub blocks_size_b {
+sub blocks_size {
     my ($self) = @_;
-    return $self->{blocks_size_b} if ($self->{blocks_size_b});
-    $self->{blocks_size_b} = ($self->blocks_size() & 16777215);
-    return $self->{blocks_size_b};
+    return $self->{blocks_size} if ($self->{blocks_size});
+    $self->{blocks_size} = ($self->blocks_size_raw() & 16777215);
+    return $self->{blocks_size};
 }
 
 sub magic {
@@ -3463,9 +3463,9 @@ sub blocks_start {
     return $self->{blocks_start};
 }
 
-sub blocks_size {
+sub blocks_size_raw {
     my ($self) = @_;
-    return $self->{blocks_size};
+    return $self->{blocks_size_raw};
 }
 
 sub blocks_end {
--- dwg_ac1_50.ksy
+++ dwg_ac1_50.ksy
@@ -42,7 +42,7 @@ seq:
     repeat-expr: header.table_view.items
   - id: block_entities
     type: real_entities
-    size: header.blocks_size_b
+    size: header.blocks_size
   - id: todo
     size-eos: true
     repeat: eos
@@ -108,7 +108,7 @@ types:
         type: u4
       - id: blocks_start
         type: u4
-      - id: blocks_size
+      - id: blocks_size_raw
         type: u4
       - id: blocks_end
         type: u4
@@ -129,10 +129,10 @@ types:
       - id: variables
         type: header_variables
     instances:
-      blocks_size_a:
-         value: (blocks_size & 0xff000000) >> 24
-      blocks_size_b:
-         value: (blocks_size & 0x00ffffff)
+      blocks_size_unknown:
+         value: (blocks_size_raw & 0xff000000) >> 24
+      blocks_size:
+         value: (blocks_size_raw & 0x00ffffff)
   table:
     seq:
       - id: item_size
